41f4391fda5e742843bb2984a7094b90d7ac8694
Valerio Evangelisti <valerio.evangelisti@gucci.com>
2024-03-19 10:20:24 +0100

    setup monorepo + fleet manager app

---
+++ fleet-manager/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'fleet-manager',
+  preset: '../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../coverage/fleet-manager',
+};
