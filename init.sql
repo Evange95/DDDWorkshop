5eb3362d6ca2d5dc2d26057df235c7a0ba09ad2d
Valerio Evangelisti <valerio.evangelisti@gucci.com>
2024-03-21 10:02:27 +0100

    aligned to basepoint

--- init.sql
+++
@@ -1,5 +0,0 @@
-USE mysql;
-CREATE DATABASE IF NOT EXISTS evilton;
-USE evilton;
-CREATE TABLE IF NOT EXISTS aircrafts (Model VARCHAR(255) PRIMARY KEY, Manufacturer VARCHAR(255), Wingspan INT, `CabinWidth` FLOAT, `CabinHeight` FLOAT, `CabinLength` FLOAT, `CargoCapacity` INT, `Range` INT, `CruiseSpeed` INT, `EngineType` VARCHAR(255), `NoiseLevel` VARCHAR(255), `Version` INT);
-CREATE TABLE IF NOT EXISTS seatTypes (SeatTypeID INT PRIMARY KEY, SeatType VARCHAR(255), Width FLOAT, Height FLOAT, Pitch FLOAT, Weight FLOAT, ProductionDate DATE, ComfortLevel INT, Features VARCHAR(255), `Version` INT)
